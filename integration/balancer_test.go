3b421f206e76d6d01c384e4db59f0bd9f15ad2b7
LikerFeed <vanya18845@gmail.com>
2023-06-01 00:18:09 +0300

    added integration tests

--- integration/balancer_test.go
+++ integration/balancer_test.go
@@ -6,6 +6,9 @@ import (
 	"os"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
 const baseAddress = "http://balancer:8090"
@@ -14,19 +17,42 @@ var client = http.Client{
 	Timeout: 3 * time.Second,
 }
 
-func TestBalancer(t *testing.T) {
-	if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
-		t.Skip("Integration test is not enabled")
-	}
-
-	// TODO: Реалізуйте інтеграційний тест для балансувальникка.
-	resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
-	if err != nil {
-		t.Error(err)
-	}
-	t.Logf("response from [%s]", resp.Header.Get("lb-from"))
+type IntegrationTestSuite struct {
+	suite.Suite
 }
 
-func BenchmarkBalancer(b *testing.B) {
-	// TODO: Реалізуйте інтеграційний бенчмарк для балансувальникка.
+func TestBalancer(t *testing.T) {
+	suite.Run(t, new(IntegrationTestSuite))
+}
+
+func (s *IntegrationTestSuite) TestGetRequest() {
+	if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
+		s.T().Skip("Integration test is not enabled")
+	}
+
+	serverNum := 0
+	for i := 0; i < 10; i++ {
+		resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
+		assert.NoError(s.T(), err)
+
+		if i%3 == 0 {
+			serverNum = 1
+		} else if i%3 == 1 {
+			serverNum = 2
+		} else {
+			serverNum = 3
+		}
+		assert.Equal(s.T(), fmt.Sprintf("server%d:8080", serverNum), resp.Header.Get("lb-from"))
+	}
+}
+
+func (s *IntegrationTestSuite) BenchmarkBalancer(b *testing.B) {
+	if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
+		s.T().Skip("Integration test is not enabled")
+	}
+
+	for i := 0; i < b.N; i++ {
+		_, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
+		assert.NoError(s.T(), err)
+	}
 }
