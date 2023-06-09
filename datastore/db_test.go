d01a358cffdac623119afc9b0b24a6c8d5fee16b
LikerFeed <vanya18845@gmail.com>
2023-06-09 23:35:12 +0300

    changed DB structure, added first tests

--- datastore/db_test.go
+++ datastore/db_test.go
@@ -4,6 +4,7 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strconv"
 	"testing"
 )
 
@@ -14,35 +15,39 @@ func TestDb_Put(t *testing.T) {
 	}
 	defer os.RemoveAll(dir)
 
+	const outFileSize int64 = 200
+
 	db, err := NewDb(dir)
 	if err != nil {
 		t.Fatal(err)
 	}
 	defer db.Close()
 
-	pairs := [][]string {
+	pairs := [][]string{
 		{"key1", "value1"},
 		{"key2", "value2"},
 		{"key3", "value3"},
 	}
 
-	outFile, err := os.Open(filepath.Join(dir, outFileName))
+	segmentPath := filepath.Join(dir, db.segmentName+strconv.Itoa(db.segmentNumber))
+	outFile, err := os.Open(segmentPath)
 	if err != nil {
 		t.Fatal(err)
 	}
+	defer outFile.Close()
 
 	t.Run("put/get", func(t *testing.T) {
 		for _, pair := range pairs {
 			err := db.Put(pair[0], pair[1])
 			if err != nil {
-				t.Errorf("Cannot put %s: %s", pairs[0], err)
+				t.Errorf("ERROR! Can't put %s: %s", pair[0], err)
 			}
 			value, err := db.Get(pair[0])
 			if err != nil {
-				t.Errorf("Cannot get %s: %s", pairs[0], err)
+				t.Errorf("ERROR! Can't get %s: %s", pair[0], err)
 			}
 			if value != pair[1] {
-				t.Errorf("Bad value returned expected %s, got %s", pair[1], value)
+				t.Errorf("ERROR! Bad value returned expected %s, got %s", pair[1], value)
 			}
 		}
 	})
@@ -57,15 +62,15 @@ func TestDb_Put(t *testing.T) {
 		for _, pair := range pairs {
 			err := db.Put(pair[0], pair[1])
 			if err != nil {
-				t.Errorf("Cannot put %s: %s", pairs[0], err)
+				t.Errorf("ERROR! Can't put %s: %s", pair[0], err)
 			}
 		}
 		outInfo, err := outFile.Stat()
 		if err != nil {
 			t.Fatal(err)
 		}
-		if size1 * 2 != outInfo.Size() {
-			t.Errorf("Unexpected size (%d vs %d)", size1, outInfo.Size())
+		if size1*2 != outInfo.Size() {
+			t.Errorf("ERROR! Unexpected size (%d vs %d)", size1, outInfo.Size())
 		}
 	})
 
@@ -81,12 +86,80 @@ func TestDb_Put(t *testing.T) {
 		for _, pair := range pairs {
 			value, err := db.Get(pair[0])
 			if err != nil {
-				t.Errorf("Cannot put %s: %s", pairs[0], err)
+				t.Errorf("ERROR! Can't get %s: %s", pair[0], err)
 			}
 			if value != pair[1] {
-				t.Errorf("Bad value returned expected %s, got %s", pair[1], value)
+				t.Errorf("ERROR! Bad value returned expected %s, got %s", pair[1], value)
 			}
 		}
 	})
 
+	pairs2 := [][]string{
+		{"keyA", "valueA"},
+		{"keyB", "valueB"},
+		{"keyC", "valueC"},
+		{"keyD", "valueD"},
+		{"keyA", "newA"},
+		{"keyB", "newB"},
+		{"keyC", "newC"},
+	}
+
+	t.Run("create new out file when the previous file approximately reached the expected size", func(t *testing.T) {
+		db.segmentSize = outFileSize
+		for _, pair := range pairs2 {
+			err := db.Put(pair[0], pair[1])
+			if err != nil {
+				t.Errorf("ERROR! Can't put %s: %s", pair[0], err)
+			}
+		}
+
+		files, err := ioutil.ReadDir(dir)
+		if err != nil {
+			t.Fatalf("ERROR! Unexpected error: %v", err)
+		}
+		if len(files) != 2 {
+			t.Errorf("ERROR!\nExpected: 2;\nGot %d;", len(files))
+		}
+	})
+
+	t.Run("get, if db has more than one file", func(t *testing.T) {
+		value, err := db.Get(pairs2[5][0])
+		if err != nil {
+			t.Errorf("ERROR! Can't get %s: %s", pairs2[5][0], err)
+		}
+		if value != pairs2[5][1] {
+			t.Errorf("ERROR! Bad value returned\nExpected: %s\nGot: %s", pairs2[5][1], value)
+		}
+
+		value, err = db.Get(pairs[0][0])
+		if err != nil {
+			t.Errorf("ERROR! Can't get %s: %s", pairs[0][0], err)
+		}
+		if value != pairs[0][1] {
+			t.Errorf("ERROR! Bad value returned\nExpected: %s;\nGot: %s", pairs[0][1], value)
+		}
+	})
+
+	/*t.Run("merge", func(t *testing.T) {
+		for _, pair := range pairs2 {
+			err := db.Put(pair[0], pair[1])
+			if err != nil {
+				t.Errorf("ERROR! Can't put %s: %s", pair[0], err)
+			}
+		}
+		for _, pair := range pairs2 {
+			err := db.Put(pair[0], pair[1])
+			if err != nil {
+				t.Errorf("ERROR! Can't put %s: %s", pair[0], err)
+			}
+		}
+
+		files, err := ioutil.ReadDir(dir)
+		if err != nil {
+			t.Fatalf("ERROR! Unexpected error: %v", err)
+		}
+		if len(files) != 2 {
+			t.Errorf("ERROR!\nExpected: 2;\nGot: %d", len(files))
+		}
+	})*/
 }
